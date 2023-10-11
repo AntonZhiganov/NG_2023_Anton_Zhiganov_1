f10090a644f234d8afa1391b4f5cb29a74ea6f14
AntonZhiganov <Antohajigan@gmail.com>
2023-10-11 19:22:20 +0200

    add file (main.py) in Lection_1/Task_2

---
+++ lection_1/Task_2/main.py
@@ -0,0 +1,5 @@
+print ("Enter two numbers")
+nummer1 = int(input ("Enter first number: ")) #User enter 1 number
+nummer2 = int(input ("Enter second number: ")) #User enter 2 number
+print ("resultat")
+print (nummer1 + nummer2) #Schow sum
