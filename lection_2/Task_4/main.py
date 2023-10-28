89880300beb18666b299c8b01cf9c70394c0de57
AntonZhiganov <Antohajigan@gmail.com>
2023-10-28 16:17:44 +0200

    Add Task_4 Lection_2

---
+++ lection_2/Task_4/main.py
@@ -0,0 +1,27 @@
+userList = input("Enter word: ")
+print("only vowels: ")
+for letter in userList:
+    if letter == "a":
+        print("a")
+    if letter == "o":
+        print("o")
+    if letter == "u":
+        print("u")
+    if letter == "i":
+        print("i")
+    if letter == "y":
+        print("y")
+    if letter == "e":
+        print("e")
+    if letter == "A":
+        print("A")
+    if letter == "O":
+        print("O")
+    if letter == "U":
+        print("U")
+    if letter == "I":
+        print("I")
+    if letter == "Y":
+        print("Y")
+    if letter == "E":
+        print("E")
