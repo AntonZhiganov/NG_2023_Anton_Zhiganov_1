ad51e432317f3bbbac20c60f0a97dc8e239b7034
AntonZhiganov <Antohajigan@gmail.com>
2023-10-29 10:07:34 +0100

    Add Task_1 Lection_3 and add file text.txt

---
+++ Lection_3/Task_1/main.py
@@ -0,0 +1,9 @@
+userFile = input("Enter full name you file: ")
+openFile = open(userFile, 'r')
+contentFile = openFile.read()
+
+result = {}
+for char in set(contentFile):
+    result.update({char : contentFile.count(char)})
+
+print(result)
